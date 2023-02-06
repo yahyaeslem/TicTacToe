b7dc473f22ffb452809de389f20d8848830d45cb
yahyaeslem <yahyaeslem@gmail.com>
2023-02-06 16:27:40 +0300

    logic player module pattern -everytime click render -

--- script.js
+++ script.js
@@ -6,59 +6,94 @@ var gameboard = [square[0].innerHTML,square[0].innerHTML,square[0].innerHTML
                 ,square[0].innerHTML,square[0].innerHTML,square[0].innerHTML
                 ,square[0].innerHTML,square[0].innerHTML,square[0].innerHTML];
 
- const sq=(index)=> gameboard[index];
+ const boardBox=(index)=> gameboard[index];
 return{
-    sq,
+    boardBox,
   }
 
 })();
 
-
+let currentPlayer;
 //Player Object
-const Player= (name)=>{
+const Player= ()=>{
     const getName =()=>  currentPlayer;
     const getScore = ()=> score;
-    // let currentPlayer = name || "playerOne"
-   
-return {getName, getScore};
+    const getSign = ()=> { 
+       getName()
+      if(currentPlayer === "playerTwo")
+      {currentPlayer ="playerOne"
+      return playerTwoSign;
+      
+      }
+      else
+      {
+      currentPlayer="playerTwo"
+      return playerOneSign ;
+      }
+  }
+  
+    let playerOneSign = "X";
+    let playerTwoSign = "O";   
+    
+    let playerOne = "playerOne"
+    let playerTwo = "playerTwo"
+return {getName, getSign};
 }
 // let currentPlayer = Player("playerOne").getName()
 
-let currentPlayer;
-// function to reach Gameboard object
 
-for(let i=0 ; i<9; i++){
-    
-    
-    square[i].innerHTML = (Gameboard.sq(i))
+
+// function to reach Gameboard object
+var displayController = (()=>{
+const _renderDisplay= () => Player().getSign();
+
+
+return{ _renderDisplay
 }
+})();
+
   
 // Event listerer for click and render "X" "O" 
-  document.addEventListener("click", function(e){
+  // document.addEventListener("click", function(e){
    
-    const target = e.target.closest(".square"); 
-    if(target){
+  //   const target = e.target.closest(".square"); 
+  //   if(target){
        
     
-      if(currentPlayer === "playerTwo")
-       {
-        if(e.target.innerHTML === "")
-        {
-            e.target.innerHTML = "O"
+  //     if(currentPlayer === "playerTwo")
+  //      {
+  //       if(e.target.innerHTML === "")
+  //       {
+  //           e.target.innerHTML = "O"
             
-        }
-        currentPlayer="playerOne"
-      }  
-      else
-      {
-          if(e.target.innerHTML === "")
-          {
-              e.target.innerHTML = "X"
-              currentPlayer = "playerTwo"
-          };
-      }
+  //       }
+  //       currentPlayer="playerOne"
+  //     }  
+  //     else
+  //     {
+  //         if(e.target.innerHTML === "")
+  //         {
+  //             e.target.innerHTML = "X"
+  //             currentPlayer = "playerTwo"
+  //         };
+  //     }
 
-    }
-  }
+  //   }
+  // }
   
-  );
+  // );
+
+  document.addEventListener("click", function(e){
+    const target = e.target.closest(".square"); 
+    if(target)
+    var squareID = parseInt((e.target).id)
+    {
+      if(e.target.innerHTML === "")
+      
+        {
+          
+          square[squareID].innerHTML = displayController._renderDisplay();
+        }
+  
+    }
+  });
